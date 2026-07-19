08de103cdaca05813a643c83beb58f0d85f730b9
Siddhesh Kale <55821961+sidd-sh@users.noreply.github.com>
2026-07-19 23:05:23 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -165,7 +165,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM
 git clone https://github.com/MichaelAquilina/zsh-you-should-use.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/you-should-use"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 rm -f ~/.zshrc
-cd ~/dotfiles && stow zshrc
+cd ~/dotfiles && stow zsh
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
